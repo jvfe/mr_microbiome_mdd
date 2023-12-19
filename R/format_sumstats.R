7c041c2d1af5131160b5c22dab168627d7fd7774
jvfe <jvfecav@gmail.com>
2023-12-19 09:07:38 -0300

    feat: Implement function for cleaning data

--- R/format_sumstats.R
+++ R/format_sumstats.R
@@ -1,3 +1,4 @@
+options(timeout = 10000)
 BiocManager::install("SNPlocs.Hsapiens.dbSNP155.GRCh37")
 BiocManager::install("BSgenome.Hsapiens.1000genomes.hs37d5")
 install.packages("vroom")
@@ -5,18 +6,37 @@ install.packages("vroom")
 library(MungeSumstats)
 library(vroom)
 library(dplyr)
+library(fs)
 
-phenotype <- vroom("data/head_female.tsv")
+read_ukbb_sumstat <-
+  function(filepath,
+           refgenome = "GRCh37",
+           threads = 8,
+           ...) {
+    phenotype_sumstat <- vroom(filepath)
 
-phenotype_rsids <- vroom("data/variants.tsv.bgz") %>%
-  filter(variant %in% phenotype$variant) %>%
-  right_join(phenotype, by = "variant")
+    # Get filepath, result path will be the same but .gz
+    filename <- path_ext_set(path_file(filepath), ext = "gz")
 
-gc()
+    # Join in with rsIDs and etc
+    phenotype_rsids <- vroom("data/variants.tsv.bgz") %>%
+      filter(variant %in% phenotype_sumstat$variant) %>%
+      right_join(phenotype_sumstat, by = "variant")
 
-reformatted <-
-  MungeSumstats::format_sumstats(path=phenotype_rsids,
-                                 ref_genome="GRCh37",
-                                 nThread = 8,
-                                 save_path = "results/2090_clean_female.tsv.gz")
+    gc()
 
+    reformatted <-
+      MungeSumstats::format_sumstats(
+        path = phenotype_rsids,
+        ref_genome = refgenome,
+        nThread = threads,
+        save_path = paste0("results/clean_sumstats/", filename),
+        ...
+      )
+
+    return(reformatted)
+  }
+
+sumstats_list <- dir_ls("data/neal_depression/howard_phenotypes/", glob = "*tsv.bgz")
+
+purrr::walk(sumstats_list, read_ukbb_sumstat, threads = 16)
